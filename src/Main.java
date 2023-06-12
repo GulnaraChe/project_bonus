f6a096ba62ba777882b0e8e2ae6ab836d6b00a58
Gulnara Che <g.chepurnova@gmail.com>
2023-06-12 12:49:24 +0500

    Add existing file

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 8640;
+        int priceBonusMile = 20;
+
+        System.out.println("Бонус за покупку");
+        int bm = price / priceBonusMile;
+        System.out.println(bm + " миль ");
+    }
+}
